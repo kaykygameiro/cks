8528ba6688f021cd48bbfd6cc05bfc43a7459a0c
kaykygameiro <kayky.22204708360060@faeterj-rio.edu.br>
2025-07-17 00:00:25 -0300

    menu adicionado

--- header.php
+++ header.php
@@ -45,3 +45,11 @@ $cart_count = WC()->cart->get_cart_contents_count();
 </nav>
 
 </header>
+
+<?php
+wp_nav_menu([
+  'menu' => 'categorias',
+  'container' => 'nav',
+  'container_class' => 'menu-categorias'
+])
+?>
